b2f30c0587dffbde279fdf0b681c472c0cf68c9c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-22 17:05:03 +0900

    modify reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../slices/todoSlice'
+import taskReducer from '../slices/todoSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    task: taskReducer,
   },
 })
 
