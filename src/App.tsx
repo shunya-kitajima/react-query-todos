931b77664b7fced9d61d3adab25c7f41b3c6537f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-30 01:23:11 +0900

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import MainTask from './components/MainTask'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +17,12 @@ const App: React.FC = () => {
   return (
     <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter></BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainTask />} />
+            <Route path="/tags" />
+          </Routes>
+        </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </div>
