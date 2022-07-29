1bbe4835c69239474c9bd480845627284dc77158
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-29 17:25:02 +0900

    import some component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-function App() {
-  return <div className=""></div>
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter></BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </div>
+  )
 }
 
 export default App
