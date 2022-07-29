1faa68976c83e95a03defdd463b27637e9ff42e6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-29 17:42:31 +0900

    add file

---
+++ src/hooks/useMutateTask.ts
@@ -0,0 +1,4 @@
+import axios from 'axios'
+import { useAppDispatch } from '../app/hooks'
+import { resetEditedTask } from '../slices/todoSlice'
+import { useQueryClient, useMutation } from '@tanstack/react-query'
