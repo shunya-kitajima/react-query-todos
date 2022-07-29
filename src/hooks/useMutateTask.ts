23b7fe70e1531218f361057172422b20d2b3d319
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-29 18:02:15 +0900

    add mutation

--- src/hooks/useMutateTask.ts
+++ src/hooks/useMutateTask.ts
@@ -2,3 +2,48 @@ import axios from 'axios'
 import { useAppDispatch } from '../app/hooks'
 import { resetEditedTask } from '../slices/todoSlice'
 import { useQueryClient, useMutation } from '@tanstack/react-query'
+import { Task, EditTask } from '../types/types'
+
+export const useMutate = () => {
+  const dispatch = useAppDispatch()
+  const queryClient = useQueryClient()
+
+  const createTaskMutation = useMutation(
+    (task: Omit<EditTask, 'id'>) =>
+      axios.post<Task>(`${process.env.REACT_APP_REST_URL}/tasks/`, task),
+    {
+      onSuccess: (res) => {
+        const previousTodos = queryClient.getQueryData<Task[]>(['tasks'])
+        if (previousTodos) {
+          queryClient.setQueryData<Task[]>(
+            ['tasks'],
+            [...previousTodos, res.data]
+          )
+        }
+        dispatch(resetEditedTask())
+      },
+    }
+  )
+
+  const updateTaskMutation = useMutation(
+    (task: EditTask) =>
+      axios.put<Task>(
+        `${process.env.REACT_APP_REST_URL}/tasks/${task.id}/`,
+        task
+      ),
+    {
+      onSuccess: (res, variables) => {
+        const previousTodos = queryClient.getQueryData<Task[]>(['tasks'])
+        if (previousTodos) {
+          queryClient.setQueryData<Task[]>(
+            ['tasks'],
+            previousTodos.map((task) =>
+              task.id === variables.id ? res.data : task
+            )
+          )
+        }
+        dispatch(resetEditedTask())
+      },
+    }
+  )
+}
