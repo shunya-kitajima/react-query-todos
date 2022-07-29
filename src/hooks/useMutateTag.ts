9b785f84cf10571c2a0bd0ea93d325e67140eb9f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-07-30 00:32:00 +0900

    create mutation

--- src/hooks/useMutateTag.ts
+++ src/hooks/useMutateTag.ts
@@ -3,3 +3,58 @@ import { useAppDispatch } from '../app/hooks'
 import { resetEditedTag } from '../slices/todoSlice'
 import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { Tag } from '../types/types'
+
+export const useMutateTag = () => {
+  const dispatch = useAppDispatch()
+  const queryClient = useQueryClient()
+
+  const createTagMutation = useMutation(
+    (tag: Omit<Tag, 'id'>) =>
+      axios.post<Tag>(`${process.env.REACT_APP_REST_URL}/tags/`, tag),
+    {
+      onSuccess: (res) => {
+        const previousTags = queryClient.getQueryData<Tag[]>(['tags'])
+        if (previousTags) {
+          queryClient.setQueryData<Tag[]>(['tag'], [...previousTags, res.data])
+        }
+        dispatch(resetEditedTag())
+      },
+    }
+  )
+
+  const updateTagMutation = useMutation(
+    (tag: Tag) =>
+      axios.put<Tag>(`${process.env.REACT_APP_REST_URL}/tags/${tag.id}/`, tag),
+    {
+      onSuccess: (res, variables) => {
+        const previousTags = queryClient.getQueryData<Tag[]>(['tags'])
+        if (previousTags) {
+          queryClient.setQueryData<Tag[]>(
+            ['tags'],
+            previousTags.map((tag) =>
+              tag.id === variables.id ? res.data : tag
+            )
+          )
+        }
+        dispatch(resetEditedTag())
+      },
+    }
+  )
+
+  const deleteTagMutation = useMutation(
+    (id: number) =>
+      axios.delete(`${process.env.REACT_APP_REST_URL}/tags/${id}/`),
+    {
+      onSuccess: (res, variables) => {
+        const previousTags = queryClient.getQueryData<Tag[]>(['tags'])
+        if (previousTags) {
+          queryClient.setQueryData<Tag[]>(
+            ['tags'],
+            previousTags.filter((tag) => tag.id !== variables)
+          )
+        }
+        dispatch(resetEditedTag())
+      },
+    }
+  )
+}
