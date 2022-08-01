c80ff19f3904c8e75322e4bd07f7f4a8ba9cc5b9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-01 18:49:45 +0900

    create form

--- src/components/TaskEdit.tsx
+++ src/components/TaskEdit.tsx
@@ -6,6 +6,7 @@ import { useMutateTask } from '../hooks/useMutateTask'
 
 const TaskEdit: React.FC = () => {
   const editedTask = useAppSelector(selectTask)
+  const dispatch = useAppDispatch()
   const { status, data } = useQueryTags()
   const { createTaskMutation, updateTaskMutation } = useMutateTask()
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
@@ -20,8 +21,50 @@ const TaskEdit: React.FC = () => {
       {tag.name}
     </option>
   ))
+  console.log('rendered TaskEdit')
 
-  return <div></div>
+  if (status === 'loading') return <div>{'Loading...'}</div>
+  if (status === 'error') return <div>{'Error'}</div>
+  if (createTaskMutation.isLoading) {
+    return <span>Creating...</span>
+  }
+  if (updateTaskMutation.isLoading) {
+    return <span>Updating...</span>
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          className="mb-3 px-3 py-3 border border-gray-300"
+          placeholder="new task ?"
+          type="text"
+          onChange={(e) =>
+            dispatch(setEditedTask({ ...editedTask, title: e.target.value }))
+          }
+          value={editedTask.title}
+        />
+        <button
+          className="disabled:pacity-40 my-3 mx-3 py-2 px-3 text-white bg-indig-600 hover:bg-indig-700 rouded"
+          disabled={!editedTask.title || !editedTask.tag}
+        >
+          {editedTask.id === 0 ? 'Create' : 'Update'}
+        </button>
+        <select
+          className="mb-3 px-3 py-2 border border-gray-300"
+          value={editedTask.tag}
+          onChange={(e) =>
+            dispatch(
+              setEditedTask({ ...editedTask, tag: Number(e.target.value) })
+            )
+          }
+        >
+          <option value={0}>Tag</option>
+          {tagOptions}
+        </select>
+      </form>
+    </div>
+  )
 }
 
 export default TaskEdit
