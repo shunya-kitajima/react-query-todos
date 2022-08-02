e380e6d947624ea7c64e806d18696ff57fe7b6f7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-02 18:09:36 +0900

    mofdify icon

--- src/components/MainTag.tsx
+++ src/components/MainTag.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+import { ChevronDoubleLeftIcon } from '@heroicons/react/solid'
 
 const MainTag: React.FC = () => {
   const navigate = useNavigate()
@@ -9,7 +9,7 @@ const MainTag: React.FC = () => {
   return (
     <>
       <p className="mb-10 text-xl font-bold">Tags</p>
-      <ChevronDoubleRightIcon
+      <ChevronDoubleLeftIcon
         onClick={() => navigate('/')}
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
       />
