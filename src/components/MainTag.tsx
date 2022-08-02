f7633d283473acac0ebd35c90f5d89ac6428124b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-02 17:59:32 +0900

    modify

--- src/components/MainTag.tsx
+++ src/components/MainTag.tsx
@@ -4,9 +4,11 @@ import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
 
 const MainTag: React.FC = () => {
   const navigate = useNavigate()
+  console.log('rendered MainTag')
 
   return (
     <>
+      <p className="mb-10 text-xl font-bold">Tags</p>
       <ChevronDoubleRightIcon
         onClick={() => navigate('/')}
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
