d78371f89d4a2ddbf339111ab4535b2690201235
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-02 17:51:12 +0900

    import icon and create button

--- src/components/MainTag.tsx
+++ src/components/MainTag.tsx
@@ -3,7 +3,17 @@ import { useNavigate } from 'react-router-dom'
 import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
 
 const MainTag: React.FC = () => {
-  return <div></div>
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <ChevronDoubleRightIcon
+        onClick={() => navigate('/')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p>Task page</p>
+    </>
+  )
 }
 
 export default MainTag
