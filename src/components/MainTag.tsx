18576a8cf2ab4388d28f9364b436b7fccbae657b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-02 18:52:38 +0900

    import TagList

--- src/components/MainTag.tsx
+++ src/components/MainTag.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ChevronDoubleLeftIcon } from '@heroicons/react/solid'
+import { TagListMemo } from './TagList'
 
 const MainTag: React.FC = () => {
   const navigate = useNavigate()
@@ -9,6 +10,9 @@ const MainTag: React.FC = () => {
   return (
     <>
       <p className="mb-10 text-xl font-bold">Tags</p>
+      <div className="grid grid-cols-2 gap-40">
+        <TagListMemo />
+      </div>
       <ChevronDoubleLeftIcon
         onClick={() => navigate('/')}
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
