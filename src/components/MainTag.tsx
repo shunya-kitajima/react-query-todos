2e7d03d062b2ca0c6f9dd61931801e9ae3936b25
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-08-02 17:43:38 +0900

    add file

---
+++ src/components/MainTag.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+
+const MainTag: React.FC = () => {
+  const navigate = useNavigate()
+
+  return
+  ;<div>MainTag</div>
+}
+
+export default MainTag
